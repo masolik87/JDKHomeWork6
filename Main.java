c5b35f16e282f84a53d9547a0d650760478b590a
dimon <dimon20.07@mail.ru>
2023-12-31 18:38:24 +0300

    .\Main.java

---
+++ Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.util.ArithmeticUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*Создать свой Java Maven проект;
+Добавьте зависимость commons-math3 (предназначена для решения математических задач) и изучить интерфейс библиотеки.
+Воспользоваться пакетом org.apache.commons.math3.stat и классом DescriptiveStatistics.
+Создать коллекцию из числовых элементов.
+С помощью объекта DescriptiveStatistics вычислить минимальное и максимальное значение, сумму и среднее арифметическое.
+Воспользоваться пакетом org.apache.commons.math3.util. С помощью класса ArithmeticUtils найти :
+факториал числа N.
+Наименьшее общее частное двух чисел
+Наибольший общий делитель двух чисел
+Проверить что число N это степень двойки
+*/
+
+/*Создать свой Java Gradle проект;
+Добавить зависимость Guava (популярная библиотека от Google, содержащая набор утилитарных механизмов).
+Воспользоваться утилитарным классом Lists:
+Развернуть список элементов
+Получить лист Character из строки
+Разделить лист на группы по 2 элемента
+Воспользоваться утилитарным классом Sets
+Получить итоговый Set из двух коллекций
+Получить итоговый Set из общих элементов двух коллекций
+Получить итоговый Set из непересекающихся элементов двух коллекций
+*/
+public class Main {
+    //static double[] numberList = new double[]{6d, 4d, 12d, 16d, 100d};
+
+
+
+
+    public static void main(String[] args) {
+        //System.out.println(ArithmeticUtils.factorial(3));
+        //System.out.println(ArithmeticUtils.lcm(2, 3));
+        // System.out.println(ArithmeticUtils.gcd(2, 3));
+        //  System.out.println(ArithmeticUtils.isPowerOfTwo(8));
+        // DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics(numberList);
+
+        // System.out.println(descriptiveStatistics.getMean());
+
+    }
+}
